6f46fa3a657a5d8aa34efcf413a58a604abf1278
notpankaj <pankaj0409@yahoo.com>
2021-10-09 19:47:15 +0530

    core-redux-middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import reactDom from "react-dom";
+import App from "./App";
+import {createStore,applyMiddleware} from "redux"
+import {Provider} from "react-redux"
+import {counterReducer} from './reducer'
+import logger from 'redux-logger';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const myLogger = (store) => {
+//     return next => {
+//         return action => {
+//             console.log("MIDDLEWARE RAN");
+//             return next(action)
+//         }
+//     }
+// }
+
+// ### SHORT HAND ###  
+const myLogger = store => next => action => {
+        // console.log(" 1st MIDDLEWARE RAN",action);
+        return next(action);
+}
+
+const secondMiddleware = store => next => action => {
+    // console.log("2nd MIDDLEWARE RAN",action);
+    return next(action);
+}
+
+const capAtTen = store => next => action => {
+    //modifing action on the fly
+    if(store.getState() >= 10){
+        return next({type : "DECREMENT"});
+    }
+    return next(action);
+}
+
+/* ##########################################
+                Third party middlewares 
+   ##########################################*/
+//  npm i redux-logger
+
+
+
+const store = createStore(counterReducer,applyMiddleware(myLogger,secondMiddleware,capAtTen,logger));
+
+reactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
